a99d9e44b054aaf007ed99ec44af38bd64d78750
Thassya <thassya@ice.ufjf.br>
2025-11-22 18:42:16 -0300

    criando Quantidade como Objeto de Valor

---
+++ InventoryManagement.Domain/ValueObjects/Quantidade.cs
@@ -0,0 +1,45 @@
+using InventoryManagement.Domain.Common;
+using InventoryManagement.Domain.Enums;
+
+namespace InventoryManagement.Domain.ValueObjects
+{
+    public sealed class Quantidade : ValueObject
+    {
+        public decimal Valor { get; }
+        public UnidadeMedida Unidade { get; }
+
+        public Quantidade(decimal valor, UnidadeMedida unidadeMedida)
+        {
+            if (valor <= 0)
+                throw new ArgumentException("Quantidade deve ser maior que zero para existir.");
+
+            Valor = valor;
+            Unidade = unidadeMedida;
+        }
+
+        public Quantidade Adicionar(decimal valorASerAdicionado)
+        {
+             if (Valor <= 0)
+                throw new ArgumentException("Quantidade deve ser maior que zero.");
+                ßß
+            return new Quantidade(Valor + valorASerAdicionado, Unidade);
+        }
+
+        public Quantidade Remover(decimal valorASerRemovido)
+        {
+            if (Valor <= 0)
+                throw new ArgumentException("Quantidade deve ser maior que zero.");
+
+            if (Valor - valorASerRemovido < 0)
+                throw new InvalidOperationException($"Quantidade insuficiente. Quantidade disponível: {Valor}");
+
+            return new Quantidade(Valor - valorASerRemovido, Unidade);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Valor;
+            yield return Unidade;
+        }
+    }
+}
